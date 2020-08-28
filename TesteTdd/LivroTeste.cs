cf665bd77cedca18108d04322e17ea8935cf3c7f
Cassio <cassio_de_vargas@yahoo.com.br>
2020-08-28 11:23:37 -0300

    commit dos testes

---
+++ TesteTdd/LivroTeste.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProvaSoftDesign.Models;
+using ProvaSoftDesign.Models.Entity;
+using ProvaSoftDesign.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteTdd
+{
+    [TestClass]
+    public class LivroTeste
+    {
+        [TestMethod]
+        public void AlugaLivroNok()
+        {
+            IQueryable<Livro> livrosLista = new List<Livro> { new Livro() { Id = 1, Alugado = true, Ativo = true, AutorLivro = "Eu", DataLancamento = DateTime.Now, NomeLivro = "Livro do lobo" } }.AsQueryable<Livro>();
+
+            var mockSet = new Mock<DbSet<Livro>>();
+            mockSet.As<IQueryable<Livro>>().Setup(s => s.Provider).Returns(livrosLista.Provider);
+            mockSet.As<IQueryable<Livro>>().Setup(s => s.Expression).Returns(livrosLista.Expression);
+            mockSet.As<IQueryable<Livro>>().Setup(s => s.ElementType).Returns(livrosLista.ElementType);
+            mockSet.As<IQueryable<Livro>>().Setup(s => s.GetEnumerator()).Returns(livrosLista.GetEnumerator());
+
+
+            var mockContexto = new Mock<ProvaContext>();
+            mockContexto.Setup(s => s.Livros).Returns(mockSet.Object);
+
+            LivroNegocio livroNegocio = new LivroNegocio(mockContexto.Object);
+
+            bool alugou = livroNegocio.AlugaLivro(1);
+
+            Assert.IsFalse(alugou);
+        }        
+    }
+}
---
+++ TesteTdd/UsuarioTeste .cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ProvaSoftDesign.Models;
+using ProvaSoftDesign.Models.Entity;
+using ProvaSoftDesign.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteTdd
+{
+    [TestClass]
+    public class UsuarioTeste
+    {
+        [TestMethod]
+        public void LoginNok()
+        {
+            IQueryable<Usuario> UsuariosLista = new List<Usuario> { new Usuario() { Id = 1, Login = "admin", Senha = "admin" } }.AsQueryable<Usuario>();
+
+            var mockSet = new Mock<DbSet<Usuario>>();
+            mockSet.As<IQueryable<Usuario>>().Setup(s => s.Provider).Returns(UsuariosLista.Provider);
+            mockSet.As<IQueryable<Usuario>>().Setup(s => s.Expression).Returns(UsuariosLista.Expression);
+            mockSet.As<IQueryable<Usuario>>().Setup(s => s.ElementType).Returns(UsuariosLista.ElementType);
+            mockSet.As<IQueryable<Usuario>>().Setup(s => s.GetEnumerator()).Returns(UsuariosLista.GetEnumerator());
+
+
+            var mockContexto = new Mock<ProvaContext>();
+            mockContexto.Setup(s => s.Usuario).Returns(mockSet.Object);
+
+            UsuarioNegocio usuarioNegocio = new UsuarioNegocio(mockContexto.Object);
+
+            bool valido = usuarioNegocio.ValidaUsuarioSenha("admin","teste");
+
+            Assert.IsFalse(valido);
+        }        
+    }
+}
